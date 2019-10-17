dfcac4b415e92dfc7d052ced387ff6670071e2d7
gooking <javazj@qq.com>
2019-10-17 16:58:03 +0800

    文章列表页面改为读取后天文章列表

--- photography/pages/classify/classify.js
+++ photography/pages/classify/classify.js
@@ -1,18 +1,53 @@
-// pages/classify/classify.js
+const WXAPI = require('apifm-wxapi')
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-  
+    page: 1, // 读取第几页数据，便于实现下滑分页
+    articleList: [] // 文章列表
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
-  
+  async onLoad (options) {
+    // 读取分类详情
+    const categoryInfo = await WXAPI.cmsCategoryDetail(options.pid);
+    if (categoryInfo.code != 0) {
+      wx.showModal({
+        title: '提示',
+        content: '当前分类不存在',
+        showCancel: false,
+        confirmText: '返回',
+        success(res) {
+          wx.navigateBack()
+        }
+      })
+      return;
+    }
+    // 设置小程序名称
+    wx.setNavigationBarTitle({
+      title: wx.getStorageSync('mallName')
+    })    
+    this.setData({
+      categoryInfo: categoryInfo.data
+    });
+    // 读取分类下的文章
+    this.fetchArticles(options.pid);
+  },
+
+  async fetchArticles (pid) {
+    const response = await WXAPI.cmsArticles({
+      page: this.data.page,
+      categoryId: pid
+    });
+    if (response.code == 0) {
+      this.setData({
+        articleList: this.data.articleList.concat(response.data)
+      });
+    }
   },
 
   /**
